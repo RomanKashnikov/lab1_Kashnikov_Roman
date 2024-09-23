6c86bebb2bbc7bbde2269467aee16b006bc1e7a2
RomanKashnikov <kashnikov05@list.ru>
2024-09-23 13:35:36 +0300

    Меню и выбор функции
    Создал меню в main, задал каждому значению функцию

--- Kashnikov_lab1/Kashnikov_lab1/Kashnikov_lab1.cpp
+++ Kashnikov_lab1/Kashnikov_lab1/Kashnikov_lab1.cpp
@@ -1,20 +1,99 @@
-﻿// Kashnikov_lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <typeinfo>
 
-#include <iostream>
+
+using namespace std;
+
+
+void add_pipe(int& x) //1
+{
+    cout << "ovjnjlenrve";
+}
+
+
+void add_ORS(int& x) //2
+{
+    cout << "ovjnjlenrve";
+}
+
+
+void all_objects(int& x) //3
+{
+    cout << "ovjnjlenrve";
+}
+
+
+void edit_pipe(int& x) //4
+{
+    cout << "ovjnjlenrve";
+}
+
+
+void edit_ORS(int& x) //5
+{
+    cout << "ovjnjlenrve";
+}
+
+
+void save(int& x) //6
+{
+    cout << "ovjnjlenrve";
+}
+
+
+void install(int& x) //7
+{
+    cout << "ovjnjlenrve";
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    while (1) {
+        cout << "Menu:" << endl;
+        cout << "1 - Add a pipe" << endl;
+        cout << "2 - Add a ORS(Oil Refinery Station)" << endl;
+        cout << "3 - View all objects" << endl;
+        cout << "4 - Edit pipe" << endl;
+        cout << "5 - Edit ORS(Oil Refinery Station)" << endl;
+        cout << "6 - Save" << endl;
+        cout << "7 - Install" << endl;
+        cout << "0 - Exit\n" << endl;
+
+        int x;
+        cin >> x;
+
+        switch (x) {
+        case 0:
+            cout << "App is closed" << endl;
+            return 0;
+        case 1:
+            add_pipe(x);
+            break;
+        case 2:
+            add_ORS(x);
+            break;
+        case 3:
+            all_objects(x);
+            break;
+        case 4:
+            edit_pipe(x);
+            break;
+        case 5:
+            edit_ORS(x);
+            break;
+        case 6:
+            save(x);
+            break;
+        case 7:
+            install(x);
+            break;
+        default:
+            cout << "There is no such value in the list, select an option from menu:\n" << endl;
+            main();
+            break;
+        }
+    }
 }
+    
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
